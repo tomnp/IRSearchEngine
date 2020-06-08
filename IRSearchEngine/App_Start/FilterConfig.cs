8375bef90674d0fc383d0c4fcccf804bfd2c003c
Phat Nghi <npn19998@gmail.com>
2020-06-08 19:54:08 +0700

    init

---
+++ IRSearchEngine/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace IRSearchEngine
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ IRSearchEngine/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IRSearchEngine.MvcApplication" Language="C#" %>
